3b3332fa9ff50ebf68edefa1af072eb24a97cc78
Xiaofan.Cong <187384424@qq.com>
2017-05-09 10:47:02 +0800

    修改fogwarn为fogworn,修改interface为desun
    修改fogwarn为fogworn,修改interface为desun

--- src/com/desun/webservice/TestWs.java
+++ src/com/desun/webservice/TestWs.java
@@ -13,7 +13,7 @@ public class TestWs {
 		// 注册WebService接口
 		factory.setServiceClass(DesunService.class);
 		// 设置WebService地址
-		factory.setAddress("http://127.0.0.1:8080/fogworn/services/interface.ws?wsdl");
+		factory.setAddress("http://127.0.0.1:8080/fogworn/services/desun.ws?wsdl");
 
 		DesunService desunService = (DesunService) factory.create();
 
